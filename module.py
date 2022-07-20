e86477777c4f29121a9100570ae69beb4cd99755
Rohan-sharma13 <rohansharma21062001@gmail.com>
2022-07-20 11:38:16 +0530

    Add existing File

--- module.py
+++ module.py
@@ -1,1 +1,10 @@
-def f_date_and_time()
+def f_date_time(a,b,c,d,e) :
+    Date_diff=a-20
+    month_diff=b-7
+    year_diff=c-2022
+    time_hrs_diff=d-11
+    time_mintue_diff=e-14
+    print("Difference Betweem Dates = ",a,"/",b,"/",c,"-","20 / 7 /2022",":",Date_diff,"/",month_diff,"/",year_diff)
+    print("Difference BEtween Time = ","(",d,":",e,")","-","( 11 : 14 )",":",time_hrs_diff,":",time_mintue_diff)
+    
+    
---
+++ modulecall.py
@@ -0,0 +1,8 @@
+a=int(input("Enter Current Date:"))
+b=int(input("Enter Current Month:"))
+c=int(input("Enter Current Year:"))
+d=int(input("Enter Current Time Hours:"))
+e=int(input("Enter Current Time Minutes:"))
+import module
+result=module.f_date_time(a,b,c,d,e)
+print(result)
