2289f5c0f0d5331438d4827a13ef6f5e19d5adc3
Rohan-sharma13 <rohansharma21062001@gmail.com>
2022-07-21 12:00:24 +0530

    Add existing file

--- day_7.py
+++ day_7.py
@@ -1,5 +1,10 @@
 l1=["m","n"]
 n=3
-a=l1[0]
-b=(1)*(int(l1[0]))
-print(b)
+l2=[]
+for i in range(0,3):
+    a=l1[0]+str(i+1)
+    l2.append(a)
+for j in range(0,3):
+    b=l1[1]+str(j+1)
+    l2.append(b)
+print(l2)
