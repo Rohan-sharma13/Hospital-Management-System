81a7d0dfa66fe766ac34674895dec8fb38a80ad1
Rohan-sharma13 <rohansharma21062001@gmail.com>
2022-07-21 12:08:07 +0530

    Add existing file

--- assignment-2/12.Program to find  sum of all elements in list.py
+++ assignment-2/12.Program to find  sum of all elements in list.py
@@ -4,6 +4,5 @@ for i in range(0,a):
     print("Enter",i+1,"Element of List:")
     b=int(input())
     list1.append(b)
-for j in range(0,len(list1)):
-    total=sum(list1)
+total=sum(list1)
 print("Sum of All Elments of List:",total)
